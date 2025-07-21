ed88d0cce7347a6da2047f85e8dc0f257b29bc39
momi <momi3355@hotmail.com>
2025-07-21 10:37:07 +0900

    프로젝트 생성

---
+++ server/src/main/java/com/olivin/app/OlivinApplication.java
@@ -0,0 +1,13 @@
+package com.olivin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlivinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlivinApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/olivin/app/OlivinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlivinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
