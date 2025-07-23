d7b07933e3e2669ffa1664814436c9a51b1d945b
Young Sun <rladudtjs5678@naver.com>
2025-07-23 17:42:49 +0900

    거래처원장 2025.07.23

---
+++ client/src/views/account/AccountLeder.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SearchTable from '@/components/common/SearchTable.vue';
+
+const filters = [
+  {}
+];
+const items = [];
+const header = [];
+
+function searchData(options) {
+  
+}
+
+</script>
+
+<template>
+  <SearchTable :filters="filters" :items="items" :header="header" @searchData="searchData"></SearchTable>
+</template>
