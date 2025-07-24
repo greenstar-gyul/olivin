63904df5ca3238b35b1185128b490e49cc89d6c2
greenstar-gyul <wasdzxcv@naver.com>
2025-07-24 17:50:23 +0900

    Axios 및 MyBatis 설정 추가, 예제 서비스 구현 / 20250724

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
+const server = 'http://localhost:3049/';
 // https://vitejs.dev/config/
 export default defineConfig({
     optimizeDeps: {
@@ -19,6 +20,17 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        port: 5173,
+        host: 'localhost',
+        proxy: {
+            '^/api': {
+                target: server,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '/')
+            }
         }
     }
 });
