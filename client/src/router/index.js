b54392b6ad071d263d8fc92769acddea2f9a94ed
tmm2176 <studybasic2176@gmail.com>
2025-07-25 12:03:49 +0900

    초기세팅/250725

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -129,6 +129,14 @@ const router = createRouter({
                     name: 'test5',
                     component: () => import('@/views/test/InputMultiTablePage.vue')
                 },
+
+                /* inventory */
+                {
+                    path: '/outbndMgmt',
+                    name: 'outbndMgmt',
+                    component: () => import('@/views/inventory/outbndMgmt.vue')
+                },
+                /* end of inventory */
             ]
         },
         {
